aa93b237ab2102fa7f0c73763d5ce8048bdb9d93
Malte Schlüter <mschlueter@eos-uptrade.de>
2020-03-21 19:17:25 +0100

    #10 Add first version of things controller

---
+++ src/Controller/ThingController.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Dto\Thing;
+use App\Repository\ThingRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class ThingController
+{
+    private NormalizerInterface $normalizer;
+    private ThingRepository $thingRepository;
+
+    public function __construct(
+        NormalizerInterface $normalizer,
+        ThingRepository $thingRepository
+    ) {
+        $this->normalizer = $normalizer;
+        $this->thingRepository = $thingRepository;
+    }
+
+    /**
+     * @Route(
+     *     "/things",
+     *     methods={"GET"}
+     * )
+     */
+    public function all(): JsonResponse
+    {
+        $things = $this->thingRepository->findAll();
+
+        $response = ['things' => []];
+
+        foreach ($things as $thing) {
+            $response['things'][] = $this->normalizer->normalize(Thing::createFromThing($thing));
+        }
+
+        return new JsonResponse($response);
+    }
+}
