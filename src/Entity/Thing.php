c75f9c5976fbed74541fb4b5e9795c9bbb8cb9c5
Boris Klotz <boris.klotz@pro-volution.de>
2020-03-21 13:17:41 +0100

    added user provider and password encoder, hashed password in fixtures

--- src/Entity/Thing.php
+++ src/Entity/Thing.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\Uuid;
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -92,8 +91,9 @@ class User implements UserInterface
         return $this;
     }
 
-    public function getSalt()
+    public function getSalt(): string
     {
+        return '';
     }
 
     public function eraseCredentials(): void
