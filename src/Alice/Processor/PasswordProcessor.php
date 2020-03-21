e7721d083e087d9478ddc34978b43edfe909d8b9
Max Girkens <maxgirkens@gmail.com>
2020-03-21 13:27:47 +0100

    alice encoder

---
+++ src/Alice/Processor/PasswordProcessor.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace App\Alice\Processor;
+
+use App\Entity\User;
+use Fidry\AliceDataFixtures\ProcessorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class PasswordProcessor implements ProcessorInterface
+{
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * Processes an object before it is persisted to DB.
+     *
+     * @param string $id Fixture ID
+     * @param object $object
+     */
+    public function preProcess(string $id, $object): void
+    {
+        if (!$object instanceof User) {
+            return;
+        }
+
+        $password = $this->passwordEncoder->encodePassword($object, $object->getPassword());
+        $object->setPassword($password);
+    }
+
+    /**
+     * Processes an object after it is persisted to DB.
+     *
+     * @param string $id Fixture ID
+     * @param object $object
+     */
+    public function postProcess(string $id, $object): void
+    {
+    }
+}
