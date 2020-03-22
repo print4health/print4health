44ecceb4c952bd3c90e3a2031f650a84fb8bb7a6
HayWo <nct-pege@web.de>
2020-03-22 02:20:25 +0100

    adds some mock content and stylling to default page

--- assets/js/container/index/index.js
+++ assets/js/container/index/index.js
@@ -4,7 +4,80 @@ class Index extends React.Component {
   render() {
     return (
       <div className="container startpage">
-        <h2>print4health</h2>
+        <h2>print4health.org</h2>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+            </div>
+          </div>
+          <div className="row"></div>
+          <div className="row">
+            <div className="col-sm card m-3">
+              Bist du Krankenhaus?<br/>
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+            </div>
+            <div className="col-sm card m-3">
+              Hast du Drucker?<br/>
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+            </div>
+            <div className="col-sm card m-3">
+              Kannst du designen?<br/>
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+            </div>
+            <div className="col-sm card m-3">
+              Bist du Medi?<br/>
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+              lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum
+            </div>
+          </div>
+          </div>
       </div>
     );
   }
