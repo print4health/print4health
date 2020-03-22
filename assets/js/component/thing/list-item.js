3cca68a59055a58661ca1067576eb31d9a0cd711
Max Girkens <maxgirkens@gmail.com>
2020-03-22 12:33:42 +0100

    proptype validation

--- assets/js/component/thing/list-item.js
+++ assets/js/component/thing/list-item.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ThingListItem extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  static get propTypes() {
+    return {
+      thing: PropTypes.object,
+    };
+  }
+
 
   render() {
     if (this.props.thing === undefined) {
