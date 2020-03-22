39e3620c61bd267c5ab80c9b3ac62fffa16cba00
HayWo <haywo@nct08.de>
2020-03-22 22:09:46 +0100

    adding first faq content

--- assets/js/container/faq/faq.js
+++ assets/js/container/faq/faq.js
@@ -5,21 +5,169 @@ class Faq extends React.Component {
     return (
       <div className="container startpage">
         <h2>FAQ</h2>
-        <span> Hier findet Ihr Antworten auf Eure Fragen.</span>
+        <h3> Hier findet Ihr Antworten auf Eure Fragen.</h3>
         <div className="container-fluid">
           <div className="row">
             <div className="col">
               <div className="card shadow-sm">
-                <div className="card-body">
+                <div className="card-header">
                   <h5 className="card-title">FAQ Krankenhaus</h5>
+                </div>
+                <div className="card-body">
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Ich bin Arzt, Krankenhaus, Pflege- oder Öffentliche Einrichtung.
+                        Wie kann ich Material anfragen?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      Registriert euch unter Login und hakt dabei das Kästchen _____ ab.
+                      So wissen wir, dass ihr unsere Produkte braucht und werden euch nach kurzer Verifikation einen Customer Account zur Verfügung stellen.
+                      </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Ich brauche ein sehr spezielles Bauteil, wie gehe ich am Besten vor?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      Lade Dein Bauteil mit Foto, Beschreibung, Herstellername und Spezifikation in unseren upload Bereich auf der Bedarfs-Webseite.
+                      Oder kontaktiere uns direkt über social media oder Email Verteiler.
+                      </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Wie kann ich den Prozess beschleunigen, ich brauche wirklich dringend Teile?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      Unterstützung der Behörden kann angefordert werden.
+                      </p>
+                      </div>
+                    </div>
+                  </div>
                   <p className="card-text"></p>
                 </div>
               </div>
             </div>
             <div className="col">
               <div className="card shadow-sm">
-                <div className="card-body">
+                <div className="card-header">
                   <h5 className="card-title">FAQ Maker</h5>
+                </div>
+                <div className="card-body">
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Ich habe ein 3D-Drucker als Privatperson. Kann ich auch damit unterstützen?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      Wir brauchen jede freie Druckerkapazität. Ob Industrie 3D-Drucker oder dein privater 3D-Drucker zu Hause - jeder kann helfen!
+                      </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Was bringt mir das Drucken der Modelle?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      Zunächst könnt ihr damit Leben retten!
+                      Außerdem arbeiten wir auf freiwilliger Basis, jedoch sind wir dabei, Kooperationen zu schließen, um euch für eure Arbeit mit Gutscheinen und Filament zu entlohnen.
+                      Keine Sorge: Eure jetzt schon erledigten Aufträge werden mit eingerechnet.
+                      </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Welches Filament brauche ich und wo kann ich das bestellen?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      In den Produktbeschreibungen findet Ihr die Materialspezifikationen.
+                      Hier finden sich auch Links von Anbietern für das zu benutzende Filament.
+                      </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Werden die Kosten für das Filament erstattet?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      Wir starten zunächst mit dem Druck auf freiwilliger Basis.
+                      Wir bemühen uns aber Kooperationen zu schließen, um euch für eure Arbeit mit Gutscheinen und Filament zu entlohnen.
+                      </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Wie kommt mein gedrucktes Teil an die Klinik, den Arzt oder den Endnutzer?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      Per freiwilliger Fahrer, Taxis, Behörden. Auch hier suchen wir nach freiwilligen Unterstützern.
+                      Kleinere Produkte und Mengen können natürlich auch per Post versendet werden.
+                      </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Wo kann ich neue Informationen ablegen?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      Im upload folder. Die Informationen werden von unserem Team gecheckt und auf der Seite in Kürze zur Verfügung gestellt.
+                      </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Wen kann ich kontaktieren, wenn meine Teile fertig gedruckt sind?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      Ist der Druck des Teils abgeschlossen, kannst du dies auf unser Plattform melden.
+                      Anschließend erfolgt die Organisation des Transportes, damit dein Teil schnellstmöglich zum Einsatz kommt.
+                      </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="card">
+                      <div className="card-header">
+                        <h6 className="card-title">Ich habe ein Modell gefunden, dass nützlich sein könnte, wie kann ich es bei euch einstellen?</h6>
+                      </div>
+                      <div className="card-body">
+                      <p className="card-text">
+                      Derzeit werden alle Modelle von uns händisch eingepflegt und überprüft.
+                      Auch in der Zukunft ist eine kuratierung der Inhalte sinnvoll, da wir uns ganz klar auf den Gesundheitsbereich beschränken wollen.
+                      Es wird in der Zukunft aber auch eine Registrierung für Maker und Ingenieure geben, um 3D Modelle leichter zur Verfügung stellen zu können.
+                      Habt bis dahin bitte noch etwas Geduld und wendet euch mit einer Idee an <a href="mailto:contact@print4health.org">contact@print4health.org</a>
+                      </p>
+                      </div>
+                    </div>
+                  </div>
                   <p className="card-text"></p>
                 </div>
               </div>
@@ -29,5 +177,5 @@ class Faq extends React.Component {
       </div>
     );
   }
-}
+};
 export default Faq;
