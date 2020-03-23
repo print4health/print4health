ff656e9a52594a8d8283337502ea6fe859fb855c
Robin Willig <robin.willig@pro-volution.de>
2020-03-23 18:11:50 +0100

    Search for name and description in things

--- src/Repository/ThingRepository.php
+++ src/Repository/ThingRepository.php
@@ -7,6 +7,7 @@ namespace App\Repository;
 use App\Entity\Thing;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityRepository;
 
 /**
  * @method Thing|null find($id, $lockMode = null, $lockVersion = null)
@@ -16,8 +17,36 @@ use Doctrine\Common\Persistence\ManagerRegistry;
  */
 class ThingRepository extends ServiceEntityRepository
 {
+    private ManagerRegistry $registry;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Thing::class);
+        $this->registry = $registry;
+    }
+
+    /**
+     * @param string $searchstring
+     * @return array<Thing>
+     */
+    public function searchNameDescription(string $searchstring): array
+    {
+        $builder = $this->getRepository()->createQueryBuilder('th');
+        $builder->where('th.name LIKE :searchstring');
+        $builder->orWhere('th.description LIKE :searchstring');
+        $builder->setParameter('searchstring', '%' . $searchstring . '%');
+
+        return $builder->getQuery()->getResult();
+    }
+
+    private function getRepository(): EntityRepository
+    {
+        $repository = $this->registry->getRepository(Thing::class);
+
+        if (!$repository instanceof EntityRepository) {
+            throw new \RuntimeException();
+        }
+
+        return $repository;
     }
 }
