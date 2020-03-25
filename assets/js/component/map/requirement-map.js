2be9d6eeb50730c81d4a210cce018ac488637cc3
Max Girkens <maxgirkens@gmail.com>
2020-03-25 11:57:58 +0100

    leaflet default icon webpack fix

--- assets/js/component/map/requirement-map.js
+++ assets/js/component/map/requirement-map.js
@@ -3,11 +3,22 @@ import AppContext from '../../context/app-context';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import axios from 'axios';
 import { Config } from '../../config';
+import PropTypes from 'prop-types';
+import 'leaflet-defaulticon-compatibility';
 
 class RequirementMap extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      orders: [],
+    };
+  }
+
+  static get propTypes() {
+    return {
+      thing: PropTypes.object,
+    };
   }
 
   componentDidMount() {
@@ -26,8 +37,20 @@ class RequirementMap extends React.Component {
       });
   }
 
-  renderMarkers(thing) {
-    console.log(thing.orders);
+  renderMarkers() {
+    return <>
+      {this.state.orders.map((order, id) => {
+        const requester = order.requester;
+        return <span key={id}>
+          <Marker
+            position={[requester.latitude, requester.longitude]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </span>;
+      })}
+    </>;
   }
 
   render() {
@@ -36,7 +59,7 @@ class RequirementMap extends React.Component {
       <Map center={position} zoom={6}>
         <TileLayer url="https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png"
                    attribution="https://maps.wikimedia.org" />
-        {this.renderMarkers(this.props.thing)}
+        {this.renderMarkers()}
       </Map>
     </div>;
   }
