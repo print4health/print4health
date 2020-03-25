e1b9df1734532a1597d937a9521c5b5f8d8cb11f
Marco Bartelt <marco.bartelt94@gmail.com>
2020-03-25 10:19:15 +0000

    fixes eslint

--- assets/js/component/footer/footer.js
+++ assets/js/component/footer/footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 class Footer extends React.Component {
   constructor(props) {
