d0e43265606763c1318c547617c347cc9d3a8826
Malte Schlüter <13586874+malteschlueter@users.noreply.github.com>
2020-03-26 22:06:34 +0100

    Extend and add more functional tests

--- tests/Controller/AbstractControllerTest.php
+++ tests/Controller/AbstractControllerTest.php
@@ -9,10 +9,40 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 abstract class AbstractControllerTest extends WebTestCase
 {
-    protected function logInAdmin(KernelBrowser $client): void
+    protected function logInMaker(KernelBrowser $client): void
+    {
+        $requestContent = [
+            'email' => 'maker@print4health.org',
+            'password' => 'test',
+        ];
+
+        $client->request('POST', '/login', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode($requestContent));
+    }
+
+    protected function logInRequester(KernelBrowser $client): void
+    {
+        $requestContent = [
+            'email' => 'requester@print4health.org',
+            'password' => 'test',
+        ];
+
+        $client->request('POST', '/login', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode($requestContent));
+    }
+
+    protected function logInUser(KernelBrowser $client): void
     {
         $requestContent = [
             'email' => 'user@print4health.org',
+            'password' => 'test',
+        ];
+
+        $client->request('POST', '/login', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode($requestContent));
+    }
+
+    protected function logInAdmin(KernelBrowser $client): void
+    {
+        $requestContent = [
+            'email' => 'admin@print4health.org',
             'password' => 'test',
         ];
 
