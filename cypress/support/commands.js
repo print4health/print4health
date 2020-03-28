7a4da58b169bbe02bca48b1a4a0e5b2f3f1d0705
Max Girkens <maxgirkens@gmail.com>
2020-03-28 16:23:34 +0100

    slow down slightly

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,17 @@
+const COMMAND_DELAY = 500;
+
+for (const command of ['visit', 'click', 'trigger', 'type', 'clear', 'reload', 'contains']) {
+  Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+    const origVal = originalFn(...args);
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(origVal);
+      }, COMMAND_DELAY);
+    });
+  });
+}
+
 Cypress.Commands.add('initApiRoutes', () => {
   cy.server().route('GET', '/things').as('thingsList');
 });
