6c77b227939f0010036483d5fdb8f801ee07e854
Conrad Barthelmes <conrad.barthelmes@pro-volution.de>
2020-03-28 10:44:17 +0100

    #141 installed symfony/debug-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
