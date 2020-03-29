19654dca010c120f9a5891de49bd62e29cdc1253
emptycurse <50260894+emptycurse@users.noreply.github.com>
2020-03-29 12:41:27 +0200

    Apply suggestions from code review
    typo
    
    Co-Authored-By: Max Girkens <maxgirkens@gmail.com>

--- assets/js/container/dashboard/dashbord.js
+++ assets/js/container/dashboard/dashbord.js
@@ -49,32 +49,32 @@ class Dashboard extends React.Component {
     }
 
     return(
-      <div className="Dashbord__table">
-        <div className="row Dashbord__headline-row">
-          <div className="col-md-2 Dashbord__headline">
+      <div className="Dashboard__table">
+        <div className="row Dashboard__headline-row">
+          <div className="col-md-2 Dashboard__headline">
             <h6>Produkt Bild</h6>
           </div>
-          <div className="col-md-2 Dashbord__headline">
+          <div className="col-md-2 Dashboard__headline">
             <h6>Produkt Name</h6>
           </div>
-          <div className="col-md-2 Dashbord__headline">
+          <div className="col-md-2 Dashboard__headline">
             <h6>Priorität Bestellung</h6>
           </div>
-          <div className="col-md-2 Dashbord__headline">
+          <div className="col-md-2 Dashboard__headline">
             <h6>Bedarf</h6>
           </div>
-          <div className="col-md-2 Dashbord__headline">
+          <div className="col-md-2 Dashboard__headline">
             <h6>Herstellerbestätigung</h6>
           </div>
-          <div className="col-md-2 Dashbord__headline">
+          <div className="col-md-2 Dashboard__headline">
             <h6>Bereits gedruckt</h6>
           </div>
         </div>
         {data.orders.map((order, i) => {
           return (
-            <div key={`order_${i}`} className='row Dashbord__value-row'>
+            <div key={`order_${i}`} className='row Dashboard__value-row'>
               <div className="col-md-2">
-                <div className='Dashbord__value Dashbord__value-img'>
+                <div className='Dashboard__value Dashboard__value-img'>
                   {/* eslint-disable-next-line react/jsx-no-target-blank */}
                   <a href={order.thing.url} target='_blank'>
                     <img key={`things_image_${i}`} src={order.thing.imageUrl}/>
@@ -82,7 +82,7 @@ class Dashboard extends React.Component {
                 </div>
               </div>
               <div className="col-md-2">
-                <div className='Dashbord__value'>
+                <div className='Dashboard__value'>
                   {/* eslint-disable-next-line react/jsx-no-target-blank */}
                   <a href={order.thing.url} target='_blank'>
                     <p>{order.thing.name}</p>
@@ -90,7 +90,7 @@ class Dashboard extends React.Component {
                 </div>
               </div>
               <div className="col-md-2">
-                <div className='Dashbord__value'>
+                <div className='Dashboard__value'>
                   {
                     // @TODO replace dummy value
                   }
@@ -98,17 +98,17 @@ class Dashboard extends React.Component {
                 </div>
               </div>
               <div className="col-md-2">
-                <div className='Dashbord__value'>
+                <div className='Dashboard__value'>
                   <p>{order.thing.needed}</p>
                 </div>
               </div>
               <div className="col-md-2">
-                <div className='Dashbord__value'>
+                <div className='Dashboard__value'>
                   {order.quantity - order.remaining}
                 </div>
               </div>
               <div className="col-md-2">
-                <div className='Dashbord__value'>
+                <div className='Dashboard__value'>
                   <p>{order.thing.printed}</p>
                 </div>
               </div>
@@ -123,14 +123,14 @@ class Dashboard extends React.Component {
     const { user } = this.context;
 
     return (
-      <div className="container Dashbord">
+      <div className="container Dashboard">
         <div className="row">
           <div className="col">
-            <h1>Dashbord</h1>
+            <h1>Dashboard</h1>
           </div>
         </div>
         <div className="row">
-          <div className="col Dashbord__user">
+          <div className="col Dashboard__user">
             <h6>{user.email}</h6>
           </div>
         </div>
