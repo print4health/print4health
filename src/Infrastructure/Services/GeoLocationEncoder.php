ab069c934a6a43a11c33a0652ea047a103536c55
Boy Persoon <b.persoon@lemursdev.com>
2020-04-01 14:06:37 +0200

    #246 draft of geoEncoder service.

---
+++ src/Infrastructure/Services/GeoLocationEncoder.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Services;
+
+use App\Infrastructure\Dto\Coordinates\Coordinates;
+use Symfony\Component\HttpClient\HttpClient;
+
+class GeoLocationEncoder
+{
+    private string $googleKey;
+    private string $baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?';
+
+    public function __construct(
+        string $googleApiKey
+    ) {
+        $this->googleKey = $googleApiKey;
+    }
+
+    public function geoEncodePostalCountry(
+        string $countryCode,
+        string $postalCode
+    ): Coordinates {
+        $components = [
+            'country:' . $countryCode,
+            'postal_code:' . $postalCode,
+        ];
+
+        try {
+            $client = HttpClient::create(['http_version' => '2.0']);
+
+            $response = $client->request('GET', $this->baseUrl, [
+                'query' => [
+                    'components' => implode('|', $components),
+                    'key' => $this->googleKey,
+                ],
+            ]);
+
+            $content = $response->toArray();
+            $location = $content['geometry']['location'];
+
+            return new Coordinates($location['lat'], $location['lng']);
+        } catch (\Exception $err) {
+            throw new \Exception('Status code not 200');
+        }
+    }
+}
