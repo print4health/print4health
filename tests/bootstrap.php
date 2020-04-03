37902f9268c6384b3025c45473420b291bc12f15
Daniel Badura <d.m.badura@googlemail.com>
2020-04-03 21:07:08 +0000

    use extra test database, init db on test run

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
@@ -11,3 +14,32 @@ if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
+
+$kernel = new Kernel('test', true);
+$kernel->boot();
+
+$application = new Application($kernel);
+$application->setAutoExit(false);
+
+$application->run(new ArrayInput([
+    'command' => 'doctrine:database:drop',
+    '--if-exists' => '1',
+    '--force' => '1',
+    '--quiet' => '1',
+]));
+$application->run(new ArrayInput([
+    'command' => 'doctrine:database:create',
+    '--quiet' => '1',
+]));
+$application->run(new ArrayInput([
+    'command' => 'doctrine:migration:migrate',
+    '--no-interaction' => '1',
+    '--quiet' => '1',
+]));
+$application->run(new ArrayInput([
+    'command' => 'hautelook:fixtures:load',
+    '--no-interaction' => '1',
+    '--quiet' => '1',
+]));
+
+$kernel->shutdown();
