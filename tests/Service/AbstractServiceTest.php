67a90c7b3a14b65478cf8445af49922df7e9cebe
Max Girkens <maxgirkens@gmail.com>
2020-04-03 14:36:38 +0200

    fix abstract service test

--- tests/Service/AbstractServiceTest.php
+++ tests/Service/AbstractServiceTest.php
@@ -8,13 +8,11 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 abstract class AbstractServiceTest extends KernelTestCase
 {
-    protected $container;
-
     public function setUp(): void
     {
         $kernel = self::bootKernel();
         $kernel->boot();
 
-        $this->container = $kernel->getContainer();
+        self::$container = $kernel->getContainer();
     }
 }
