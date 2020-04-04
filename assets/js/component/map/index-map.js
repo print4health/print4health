0ebcf90b4810fa379394101a79af210cf7322fae
Boy Persoon <b.persoon@lemursdev.com>
2020-04-04 20:32:06 +0200

    #268 convert Map in dumb component

--- assets/js/component/map/index-map.js
+++ assets/js/component/map/index-map.js
@@ -2,18 +2,22 @@ import React from 'react';
 import Map from './Map';
 import WithGeoLocation from '../../container/map/WithGeoLocation';
 import WithMarkers from '../../container/map/WithMarkers';
-import {MAKER, REQUESTER} from "../../constants/MapTypes";
+import PropTypes from "prop-types";
 
-class IndexMap extends React.Component {
+const IndexMap = ({types = [], filters = {}}) => {
 
-  render() {
-    return (
-      <WithMarkers types={[REQUESTER, MAKER]}>
-        <WithGeoLocation>
-          <Map zoom={6} />
-        </WithGeoLocation>
-      </WithMarkers>);
-  }
-}
+  return (
+    <WithMarkers types={types} filters={filters}>
+      <WithGeoLocation>
+        <Map zoom={6} />
+      </WithGeoLocation>
+    </WithMarkers>
+  )
+};
+
+IndexMap.propTypes = {
+  types: PropTypes.array,
+  filters: PropTypes.object
+};
 
 export default IndexMap;
