206896f2ea282745ead30c2c8425c230b4c661eb
Conrad Barthelmes <conrad.barthelmes@pro-volution.de>
2020-04-05 16:48:43 +0200

    #140 added basic tests for RequesterRegistrationController

---
+++ tests/Controller/RequesterRegistrationControllerTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+// TODO: Add test for showAction
+class RequesterRegistrationControllerTest extends AbstractControllerTest
+{
+    public function createRequesterDataProvider(): array
+    {
+        return [
+            [
+                [
+                    'email' => 'unittester-requester@print4health.org',
+                    'password' => '123465789',
+                    'name' => 'Unit Tester Requester',
+                    'addressStreet' => 'Salzstraße 123',
+                    'postalCode' => '48155',
+                    'addressCity' => 'Münster',
+                    'addressState' => 'NRW',
+                    'confirmedPlattformIsContactOnly' => true,
+                    'confirmedNoAccountability' => true,
+                    'confirmedNoCertification' => true,
+                    'confirmedNoAccountabiltyForMediation' => true,
+                    'confirmedRuleMaterialAndTransport' => true,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @group functional
+     * @dataProvider createRequesterDataProvider
+     */
+    public function testRequesterRegisterAction(array $requestContent): void
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/requester/registration', [], [], [], json_encode($requestContent));
+        $this->assertEquals(201, $client->getResponse()->getStatusCode());
+
+        $data = json_decode($client->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        $this->assertArrayHasKey('requester', $data);
+        $this->assertArrayHasKey('email', $data['requester']);
+        $this->assertArrayHasKey('id', $data['requester']);
+    }
+
+    public function createRequesterValidationErrorDataProvider(): array
+    {
+        return [
+            [
+                [
+                    'email' => '',
+                    'password' => '',
+                    'name' => '',
+                    'addressStreet' => '',
+                    'postalCode' => '',
+                    'addressCity' => '',
+                    'addressState' => '',
+                    'confirmedPlattformIsContactOnly' => false,
+                    'confirmedNoAccountability' => false,
+                    'confirmedNoCertification' => false,
+                    'confirmedNoAccountabiltyForMediation' => false,
+                    'confirmedRuleMaterialAndTransport' => false,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @group functional
+     * @dataProvider createRequesterValidationErrorDataProvider
+     */
+    public function testRequesterRegisterActionWithValidationErrors(array $requestContent): void
+    {
+        $client = static::createClient();
+
+        $this->logInAdmin($client);
+
+        $client->request('POST', '/requester/registration', [], [], [], json_encode($requestContent));
+
+        $this->assertEquals(422, $client->getResponse()->getStatusCode());
+        $data = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('type', $data);
+        $this->assertArrayHasKey('message', $data);
+        $this->assertArrayHasKey('errors', $data);
+        $this->assertCount(12, $data['errors']);
+    }
+}
