f0c6fb19ca60a17a38389c123118dcc7f627f7a9
Conrad Barthelmes <conrad.barthelmes@pro-volution.de>
2020-04-07 23:03:13 +0200

    fix migration

--- src/Infrastructure/Migrations/Version20200401192701.php
+++ src/Infrastructure/Migrations/Version20200401192701.php
@@ -22,12 +22,12 @@ final class Version20200401192701 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE `order` ADD updated_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE created_at created_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE maker ADD created_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD updated_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE requester ADD created_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD updated_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE user ADD created_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD updated_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE thing ADD created_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD updated_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE commitment ADD updated_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE created_at created_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE `order` ADD updated_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE created_at created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE maker ADD created_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD updated_date DATETIME DEFAULT NULL DEFAULT CURRENT_TIMESTAMP COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE requester ADD created_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD updated_date DATETIME DEFAULT NULL DEFAULT CURRENT_TIMESTAMP COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE user ADD created_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD updated_date DATETIME DEFAULT NULL DEFAULT CURRENT_TIMESTAMP COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE thing ADD created_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD updated_date DATETIME DEFAULT NULL DEFAULT CURRENT_TIMESTAMP COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE commitment ADD updated_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE created_at created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT \'(DC2Type:datetime_immutable)\'');
     }
 
     public function down(Schema $schema) : void
