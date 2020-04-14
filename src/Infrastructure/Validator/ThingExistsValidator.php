aa4e003705f3254b7b0c3297f883b68b3cbb7b97
Conrad Barthelmes <conrad.barthelmes@pro-volution.de>
2020-04-14 09:47:27 +0200

    php-cs-fix ??

--- src/Infrastructure/Validator/ThingExistsValidator.php
+++ src/Infrastructure/Validator/ThingExistsValidator.php
@@ -35,7 +35,8 @@ class ThingExistsValidator extends ConstraintValidator
         } catch (NotFoundException $exception) {
             $this->context->buildViolation($constraint->message)
                 ->setParameter('{{ id }}', $value)
-                ->addViolation();
+                ->addViolation()
+            ;
         }
     }
 }
