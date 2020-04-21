c639a84dff6a5e3cfec97c4a09b19539fc2ba40b
HayWo <haywo@nct08.de>
2020-04-21 15:11:24 +0200

    temprary remove language detector

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,11 +1,11 @@
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+//import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(Backend)
-  .use(LanguageDetector)
+  //.use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'de',
