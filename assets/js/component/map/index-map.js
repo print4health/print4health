2a5684314220a9a315c31fe193a324c33c58b979
Boy Persoon <b.persoon@lemursdev.com>
2020-04-22 16:21:32 +0200

    #284 Add request user strategy

--- assets/js/component/map/index-map.js
+++ assets/js/component/map/index-map.js
@@ -4,19 +4,31 @@ import WithGeoLocation from '../../container/map/WithGeoLocation';
 import WithMarkers from '../../container/map/WithMarkers';
 import PropTypes from "prop-types";
 
-const IndexMap = ({types = [], filters = []}) => {
+const IndexMap = ({types = [], filters = [], position = {}, disabled = false}) => {
+  let strategy;
+  let zoom = 6;
+
+  if('latitude' in position &&
+     'longitude' in position) {
+      strategy = "INJECTED";
+      zoom = 10;
+  }
   return (
-    <WithMarkers types={types} filters={filters}>
-      <WithGeoLocation>
-        <Map zoom={6} />
-      </WithGeoLocation>
-    </WithMarkers>
+    <div>
+      <WithMarkers types={types} filters={filters}>
+        <WithGeoLocation position={position} strategy={strategy} >
+          <Map zoom={zoom} style={true? {pointerEvents: 'none'} : {cursor: 'pointer'}}/>
+        </WithGeoLocation>
+      </WithMarkers>
+    </div>
   )
 };
 
 IndexMap.propTypes = {
   types: PropTypes.array,
-  filters: PropTypes.array
+  disabled: PropTypes.bool,
+  filters: PropTypes.array,
+  position: PropTypes.object
 };
 
 export default IndexMap;
--- assets/js/container/map/WithGeoLocation.js
+++ assets/js/container/map/WithGeoLocation.js
@@ -5,28 +5,90 @@ class WithGeoLocation extends React.Component {
 
     static get propTypes() {
         return {
+            strategy: PropTypes.string,
+            position: PropTypes.object,
             children: PropTypes.object,
         };
     }
 
-    constructor(props){
+    constructor(props) {
         super(props);
+
         this.state = {
-          strategy: "DEFAULT",
+          strategy: props.strategy || "USERREQUEST",
           latitude: 51.1642292,
           longitude: 10.4541194
         };
 
         this.getCoords = this.getCoords.bind(this);
+        this.updatePosition = this.updatePosition.bind(this);
+    }
+
+    getStrategy() {
+      return this.props.strategy || this.state.strategy;
+    }
+
+    updatePosition() {
+      try {
+        switch (this.getStrategy()) {
+          case "INJECTED":
+            this.injectedLocation();
+            return;
+          case "USERREQUEST":
+            this.requestUserLocation();
+            return;
+          default:
+            this.fallbackLocation();
+        }
+      } catch (e) {
+        this.fallbackLocation();
+      }
+    }
+
+    componentDidMount() {
+      this.updatePosition();
+    }
+
+    componentDidUpdate(prevProps) {
+        if(this.props.position !== prevProps.position ||
+           this.props.strategy !== prevProps.strategy) {
+          this.updatePosition();
+        }
+      }
+
+    requestUserLocation() {
+        navigator.geolocation.getCurrentPosition(({coords}) => {
+          this.setState({
+            latitude: coords.latitude,
+            longitude: coords.longitude});
+        });
+    }
+
+    injectedLocation() {
+        const { latitude, longitude } = this.props.position;
+
+        this.setState({
+          latitude: latitude,
+          longitude: longitude
+        });
+    }
+
+    fallbackLocation() {
+      this.setState({
+        strategy: "DEFAULT",
+        latitude: 51.1642292,
+        longitude: 10.4541194});
     }
 
     getCoords() {
-      return [
-        this.state.latitude,
-        this.state.longitude
-      ]
+        return [
+          this.state.latitude,
+          this.state.longitude
+        ]
     }
+
     render () {
+      console.log(this.getCoords());
         const childrenWithProps = React.Children.map(this.props.children, child =>
             React.cloneElement(child, {...this.props, position: this.getCoords() })
         );
