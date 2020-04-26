03cf8fb11e2bc231bdd47be45146d154968792d6
Conrad Barthelmes <conrad.barthelmes@pro-volution.de>
2020-04-26 22:01:26 +0200

    fixed bug for no translations when languagedetector is not loaded.. dont know why

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,13 +1,14 @@
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
-//import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(Backend)
-  //.use(LanguageDetector)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    lng: 'de',
     fallbackLng: 'de',
     debug: true,
     transSupportBasicHtmlNodes: true,
@@ -17,7 +18,7 @@ i18n
     },
     react: {
       wait: true,
-    },
+    }
   });
 
 export default i18n;
