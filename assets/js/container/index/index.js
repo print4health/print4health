02d3b0051989f6447c77bba596150f22a8e228f3
Conrad Barthelmes <conrad.barthelmes@pro-volution.de>
2020-04-27 21:44:53 +0200

    #290 added i18nKey to index page

--- assets/js/container/index/index.js
+++ assets/js/container/index/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AppContext from '../../context/app-context';
-import { withTranslation } from 'react-i18next';
+import { Trans, withTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 
 class Index extends React.Component {
 
   static get propTypes() {
     return {
-      t: PropTypes.func
+      t: PropTypes.func,
     };
   }
 
@@ -33,28 +33,43 @@ class Index extends React.Component {
               {t('idea.title')}
             </h3>
             <p className="mb-4">
-              {t('idea.content1')} <strong>{t('idea.highlight')}</strong> {t('idea.content2')}
+              <Trans i18nKey="page-index:idea.content">
+                Mit unserer Plattform <strong>Crowdproducing</strong>
+                soll der Einsatz von 3D-Druck zur Bekämpfung der Corana Pandemie effektiv, schnell und solidarisch
+                ermöglicht werden. Vom hochkomplexen Produkt, das individuell konstruiert werden muss, bis zu Open
+                Source Dateien für Masken, Türöffner und Wasserhahnadapter, die von jedem privaten 3D-Drucker gefertigt
+                werden können, sollen Lösungen gefunden und angeboten werden.
+              </Trans>
             </p>
             <h3 className="h4">
               <i className="far fa-arrow-alt-circle-right mr-2" />
               {t('motivation.title')}
             </h3>
             <p className="mb-4">
-              {t('motivation.content1')}
-              <br />
-              <br />
-              {t('motivation.content2')}
-              <br />
-              <br />
-              {t('motivation.content3')}
+              <Trans i18nKey="page-index:motivation.content">
+                In pandemischen Krisenzeiten gibt es im Gesundheitssektor einen großen Bedarf an Ersatzteilen. Länder,
+                in denen die Krise schon weiter fortgeschritten ist, setzen bereits auf 3D-Druck als Lösung zur
+                Bedarfsdeckung im Gesundheitssektor.
+                <br />
+                <br />
+                So unterstützen Maschinenbauer in Italien die Krankenhäuser mit 3D-gedruckten Ventilen, die
+                Beatmungsgeräte mit den Gesichtsmasken der Patienten verbinden.
+                <br />
+                <br />
+                Die polytechnische Universität Hong Kong entwickelte Schutzschilder, um das medizinische Personal vor
+                der Infizierung mit dem Coronavirus zu schützen. Auch diese wurden in hoher Stückzahl mit 3D-Druckern
+                gefertigt.
+              </Trans>
             </p>
             <h3 className="h4">
               <i className="far fa-hand-point-right mr-2" />
               {t('weneedyou.title')}
             </h3>
-            <p>
-              {t('weneedyou.content')}
-            </p>
+            <Trans i18nKey="page-index:weneedyou.content">
+              Die Plattform lebt von der stetigen Weiterentwicklung der Plattform und der Produkte. Das Know-How und die
+              Erfahrung der gesamten Community sind dabei gefragt: ob High-End Anwender mit großem Unternehmen im
+              Hintergrund, privater 3D-Drucker, Medizintechniker oder Arzt - Euer Know-How ist wertvoll und ist wichtig!
+            </Trans>
           </section>
           <section className="container py-4">
             <h2>{t('help.title')}</h2>
